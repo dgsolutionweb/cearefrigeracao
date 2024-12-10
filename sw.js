2e56163760b8aaeb617b80e92b0e632c79e8fcdf
ioursmid <dinamicasalfa@gmail.com>
2024-12-10 16:17:20 -0300

    Cartão Digital C&A Refrigeração

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'ca-refrigeracao-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/img/logo.jpg',
+    '/img/logo-192.png',
+    '/img/logo-512.png',
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request)
+                    .then((response) => {
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+                        const responseToCache = response.clone();
+                        caches.open(CACHE_NAME)
+                            .then((cache) => {
+                                cache.put(event.request, responseToCache);
+                            });
+                        return response;
+                    });
+            })
+    );
+}); 
